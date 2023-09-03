28092753f33fa7ec45e1c1aae28c6d0734c50d38
its zaki <zakiyounus789@gmail.com>
2023-09-04 04:06:22 +0500

    Color Changer

---
+++ app.js
@@ -0,0 +1,14 @@
+let body = document.querySelector("body")
+function red (){
+    body.style.backgroundColor="red"
+    
+}
+function orange (){
+    body.style.backgroundColor="orange"
+}
+function blue (){
+    body.style.backgroundColor="blue"
+}
+function green (){
+    body.style.backgroundColor="green"
+}
